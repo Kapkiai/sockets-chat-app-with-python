2ca8b829d4242e6ff992f2cb880730599ebfe9c7
Mathew Kapkiai <mathewkiprop17@gmail.com>
2020-12-27 17:55:22 +0300

    changed echo

--- echoserver.py
+++ echoserver.py
@@ -1,121 +1,33 @@
-#!/usr/bin/env python3
-# Python program to implement server side of chat room.
+#!/usr/bin/python3
+import selectors
 import socket
-import select
-import sys
-from _thread import *
 
-"""The first argument AF_INET is the address domain of the  
-socket. This is used when we have an Internet Domain with  
-any two hosts The second argument is the type of socket.  
-SOCK_STREAM means that data or characters are read in  
-a continuous flow."""
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sel = selectors.DefaultSelector()
 
+def accept(sock, mask):
+    conn, addr = sock.accept()  
+    print('accepted', conn, 'from', addr)
+    conn.setblocking(False)
+    sel.register(conn, selectors.EVENT_READ, read)
 
-IP_address = '127.0.0.1'
+def read(conn, mask):
+    data = conn.recv(1000)  
+    if data:
+        print('echoing', repr(data), 'to', conn)
+        conn.send(data)  
+    else:
+        print('closing', conn)
+        sel.unregister(conn)
+        conn.close()
 
-# takes second argument from command prompt as port number
-Port = 1237
-
-"""  
-binds the server to an entered IP address and at the  
-specified port number.  
-The client must be aware of these parameters  
-"""
-server.bind((IP_address, Port))
-
-"""  
-listens for 100 active connections. This number can be  
-increased as per convenience.  
-"""
-server.listen(100)
-
-list_of_clients = []
-clients = {}
-
-def clientthread(conn, addr):
-
-    # sends a message to the client whose user object is conn
-    welcomemsg = f"Welcome to this chatroom {clients[conn]}!"
-    conn.send(bytes(welcomemsg, 'utf-8'))
-    # print(clients[conn])
-
-    while True:
-        try:
-            message = conn.recv(2048).decode('utf-8')
-            print("message", message)
-            if message:
-
-                """prints the message and address of the  
-                user who just sent the message on the server  
-                terminal"""
-                print (f"{clients[conn]}> " + message)
-
-                # Calls broadcast function to send message to all
-                message_to_send = f"{clients[conn]}> " + message
-                broadcast(message_to_send, conn)
-
-            # else:
-            #     """message may have no content if the connection
-            #     is broken, in this case we remove the connection"""
-            #     remove(conn)
-
-        except:
-            continue
-
-
-
-"""Using the below function, we broadcast the message to all  
-clients who's object is not the same as the one sending  
-the message """
-
-
-def broadcast(message, connection):
-    for client in list_of_clients:
-        if client != connection:
-            try:
-                msg = clients[client] + message
-                clients.send(bytes(msg, 'utf-8'))
-            except:
-                clients.close()
-
-                # if the link is broken, we remove the client
-                remove(clients)
-
-
-"""The following function simply removes the object  
-from the list that was created at the beginning of  
-the program"""
-
-
-def remove(connection):
-    if connection in list_of_clients:
-        list_of_clients.remove(connection)
-
+sock = socket.socket()
+sock.bind(('localhost', 1234))
+sock.listen(100)
+sock.setblocking(False)
+sel.register(sock, selectors.EVENT_READ, accept)
 
 while True:
-
-    """Accepts a connection request and stores two parameters,  
-    conn which is a socket object for that user, and addr  
-    which contains the IP address of the client that just  
-    connected"""
-    conn, addr = server.accept()
-    user = conn.recv(1024)
-    clients[conn] = user.decode('utf-8')
-    print(clients)
-
-    """Maintains a list of clients for ease of broadcasting  
-    a message to all available people in the chatroom"""
-    list_of_clients.append(conn)
-
-    # prints the address of the user that just connected
-    print (clients[conn], " connected")
-
-    # creates and individual thread for every user
-    # that connects
-    start_new_thread(clientthread, (conn, addr))
-
-conn.close()
-server.close()
+    events = sel.select()
+    for key, mask in events:
+        callback = key.data
+        callback(key.fileobj, mask)
